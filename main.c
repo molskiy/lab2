63d8f9be8db1c3428b7562eca4761ab79e676ef2
molskiy <152178933+molskiy@users.noreply.github.com>
2023-12-02 06:07:49 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,102 +1,84 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "func.h"
-int main(){
-	perfume *perfumes = NULL;
-	int quantity=0;
-	int flag=1;
-	int tempKey=0;
-	while(flag){
-		int key = 0;
-		key += tempKey;
-		tempKey = 0;
-		if (!key){
-		    printf("Меню:\n");
-            printf("1. Создание базы данных\n");
-            printf("2. Загрузка базы данных\n");
-            printf("3. Поиск по базе данных\n");
-            printf("0. Выход\n");
-            printf("Выберите вариант: ");
-			scanf("%i", &key);
-		}
-		switch (key){
-			case 1:{
-				int key2;
-				printf("1. Создать новую базу данных\n");
-                printf("2. Загрузить существующую базу данных\n");
-				scanf("%i", &key2);
-				if (key2==1){	
-					create(&perfumes, &quantity);
-					if(perfumes==NULL){
-						printf("Ошибка создания\n");
-						flag = 0;
-						break;
-					}
-					else{
-						printf("Создано %d записей\n\n", quantity);
-					}
-					write(perfumes,quantity);
-					}
-				else if(key2==2){
-					read(&perfumes, &quantity);
-					if(perfumes==NULL){
-						printf("Ошибка чтения\n");
-						flag = 0;
-						break;
-					}
-					else
-						printf("Загружено %d записей\n\n", quantity);
-				}
-				break;
-			}
-			case 2:
-    				if(perfumes==NULL){
-    					int key3;
-    					printf("База данных не найдена. Создать новую?\n");
-    					printf("1-Да\n");
-    					printf("2-Нет\n");
-    					scanf("%i", &key3);
-    					if(key3==1) 
-    					    tempKey=1;
-    					else 
-    					    flag = 0;
-    					break;
-    				}
-    				else{
-    					for(int i = 0; i<quantity; i++){
-    				        printf("Название: %s;\n", perfumes[i].name);
-    					    printf("Цена: %f;\n", perfumes[i].price);
-    					    printf("Цвет: %s;\n", perfumes[i].colour);
-    				    	printf("\n");
-    					}
-    				}
-    			
-    			break;
-    		case 3:
-			if(perfumes==NULL) 
-					{
-						int key3;
-						printf("База данных не найдена. Создать новую?\n");
-						printf("1-Да\n");
-						printf("2-Нет\n");
-						scanf("%i", &key3);
-						if(key3==1) 
-						    tempKey=1;
-						else 
-						    flag = 0;
-						break;
-					}
-					else
-					{
-						search(perfumes,quantity);
-					}
-				break;
-			default:
-				flag = 0;
-	            break;
-		}
-	}
-	if (perfumes) 
-	    free(perfumes);
+void createDatabase() {
+    FILE *file = fopen("database.txt", "w");
+    if (file == NULL) {
+        printf("Ошибка при открытии файла\n");
+        return;
+    }
+    int quantity;
+    printf("Введите количество товаров в базе данных: ");
+    scanf("%d", &quantity);
+    Perfume *perfumes = (Perfume *) malloc(quantity * sizeof(Perfume));
+    if (perfumes == NULL) {
+        printf("Ошибка выделения памяти\n");
+        return;
+    }
+    for (int i = 0; i < quantity; i++) {
+        printf("Товар %d:\n", i + 1);
+        printf("Название: ");
+        scanf("%s", perfumes[i].name);
+        printf("Цена: ");
+        scanf("%f", &perfumes[i].price);
+        printf("Характеристики: ");
+        scanf("%s", perfumes[i].characteristics);
+    }
+    fwrite(&quantity, sizeof(int), 1, file);
+    fwrite(perfumes, sizeof(Perfume), quantity, file);
+    free(perfumes);
+    fclose(file);
+    printf("База данных успешно создана и записана в файл\n");
+}
+void loadDatabase() {
+    FILE *file = fopen("database.txt", "r");
+    if (file == NULL) {
+        printf("Ошибка при открытии файла\n");
+        return;
+    }
+    int quantity;
+    fread(&quantity, sizeof(int), 1, file);
+    Perfume *perfumes = (Perfume *) malloc(quantity * sizeof(Perfume));
+    if (perfumes == NULL) {
+        printf("Ошибка выделения памяти\n");
+        return;
+    }
+    fread(perfumes, sizeof(Perfume), quantity, file);
+    printf("База данных:\n");
+    for (int i = 0; i < quantity; i++) {
+        printf("Товар %d:\n", i + 1);
+        printf("Название: %s\n", perfumes[i].name);
+        printf("Цена: %.2f\n", perfumes[i].price);
+        printf("Характеристики: %s\n\n", perfumes[i].characteristics);
+    }
+    free(perfumes);
+    fclose(file);
+}
+int main() {
+    int choice;
+    do {
+        printf("Меню:\n");
+        printf("1. Создание базы данных\n");
+        printf("2. Загрузка базы данных\n");
+        printf("3. Вывод содержимого базы данных\n");
+        printf("4. Поиск по базе данных\n");
+        printf("0. Выход\n");
+        printf("Выберите вариант: ");
+        scanf("%d", &choice);
+        switch (choice) {
+            case 1:
+                createDatabase();
+                break;
+            case 2:
+                loadDatabase();
+                break;
+            case 3:
+                loadDatabase();
+                break;
+            case 4:
+                searchDatabase();
+                break;
+        }
+    } while (choice != 0);
     return 0;
 }
